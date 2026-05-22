c66ede4542f4a54bdfe6d944f50d6c5119e6a6dd
Arunkumar Vadivel <arunkumar.v@maxbytetech.com>
2026-05-22 16:38:33 +0530

    updated instagram mobile layout

--- src/sections/Instagram.jsx
+++ src/sections/Instagram.jsx
@@ -62,7 +62,7 @@ export default function Instagram() {
 
       {/* POSTS */}
 
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-8 mt-20">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 mt-20">
 
         {posts.map((item, index) => (
 
